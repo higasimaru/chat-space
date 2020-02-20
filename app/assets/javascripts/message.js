418ac6f923306ee4200959fc6654be76f183b76d
higasimaru <andyourbirdcansing@icloud.com>
2020-02-20 14:28:49 +0900

    message#createで保存
    messages_controller.rb
    #create
    render_to

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,12 +1,12 @@
 $(function(){
+
   $('#new_message').on('submit', function(e){
-    console.log('hoge');
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).atter('action');
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
-      type: 'POST',
+      type: "POST",
       Data: formData,
       dataType: 'json',
       processData: false,
