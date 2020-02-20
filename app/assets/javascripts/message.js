c818d806bdbdc71580cd8d076d3535aad7a94123
higasimaru <andyourbirdcansing@icloud.com>
2020-02-20 16:43:36 +0900

    create.json.jbuilder
    messageをjson形式で返す
    message.jsに中身を削除したhtmlを
    再定義

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,60 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      var html = 
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message-info">
+            <div class="message-info__name">
+              ${message.user_name}
+            </div>
+            <div class="message-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="content">
+            <p class="content__content-details">
+              ${message.content}
+            </p>       
+          </div>
+          <img src=${message.image}>
+        </div>`
+      return html;
+    } else {
+      var html = 
+        `<div class="message" data-message-id=${message.id}>
+        <div class="message-info">
+          <div class="message-info__name">
+            ${message.user_name}
+          </div>
+          <div class="message-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="content">
+          <p class="content__content-details">
+            ${message.content}
+          </p>       
+        </div>
+      </div>`
+      return html;
+    };
+  }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
-      Data: formData,
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
-    })    
-  });
+    })  
+      .done(function(data){
+        var html = buildHTML(data);
+        $
+      })
+  })
 });
