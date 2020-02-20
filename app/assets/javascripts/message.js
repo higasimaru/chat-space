fa058e3e6072e0cb73d310a6e7f664177daf9874
higasimaru <andyourbirdcansing@icloud.com>
2020-02-20 21:23:46 +0900

    .fail
    保存に失敗した時の処理を記述。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,27 +40,29 @@ $(function(){
     };
   }
 
-$('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
 
-  $('.submit-btn').removeAttr('data-disable-with');
+    $('.submit-btn').removeAttr('data-disable-with');
 
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })  
-    .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);     
-    $('form')[0].reset();
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}); 
-    
-  });
-})
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })  
+      .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);     
+      $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});       
+      })
+      .fail(function(){
+        alert("メッセージの送信に失敗しました");
+      });
+  })
 });
