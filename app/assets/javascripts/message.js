63bb077550dd19d2c6ebc15f677f3f5882b2be0c
higasimaru <andyourbirdcansing@icloud.com>
2020-02-20 13:31:11 +0900

    Ajax でメッセージ保存
    thisはform要素自体
    atterでaction属性の値を取得
    groups_group_id_massages

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,16 @@
 $(function(){
   $('#new_message').on('submit', function(e){
     console.log('hoge');
-    e.preventDefault()
-    
-  })
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).atter('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      Data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })    
+  });
 });
