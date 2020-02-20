51cf75d80e03f58a16f5a124a3c3b6ae5318da1b
higasimaru <andyourbirdcansing@icloud.com>
2020-02-20 21:11:06 +0900

    message画面スクロール、disabled解除
    chat-side__groups,chat-main__messages、にoverflow: auto;追記。
    animate,scrollTopメソッドで最下部表示。
    propでsubmit-btnをdisable解除しようとしたが、うまく当たらないのでremoveAttrで解除。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,22 +40,27 @@ $(function(){
     };
   }
 
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })  
-      .done(function(data){
-        var html = buildHTML(data);
-        $('.messages').append(html);
-        $('form')[0].reset();
-      })
-  })
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+
+  $('.submit-btn').removeAttr('data-disable-with');
+
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })  
+    .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);     
+    $('form')[0].reset();
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight}); 
+    
+  });
+})
 });
